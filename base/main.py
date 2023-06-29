18801586db14589e36a3f719bcc3fd124427676e
Wilson Forero <wilsonforerob@gmail.com>
2023-06-29 18:54:59 +0200

    Solucion problema 2

--- base/main.py
+++ base/main.py
@@ -1,19 +1,30 @@
-# TODO completa la suma
+import random
+
+
 def sum(a, b):
-    return a +
+    return a + b
 
 
-# TODO prepara una función que multiplica dos números que recibe por argumento
 def mult(a, b):
+    return a * b
 
 
-# TODO prepara una función que genere números dentro de una lista
+def generate_random_array(n):
+    random_array = []
+    for i in range(0, n):
+        random_number = random.randint(0, i)
+        random_array.append(random_number)
+    return random_array
 
 
 if __name__ == '__main__':
-    # TODO llama a la función suma y printa el resultado por pantalla
-    # TODO llama la función multiplicar y printa el resultado por pantalla
-    print(mult(, ))
-    # TODO llama a la función de generar numeros y printa la lista generada
-
-
+    # test de las funciones. Si quiere hacer mas pruebas modifique el rango en el siguiente for loop:
+    for i in range(0, 5):
+        print(f"Inicio de la prueba {i+1}:")
+        # funcion que suma dos numeros
+        print(f"Funcion suma: {sum(random.randint(i, 100), random.randint(i, 100))}")
+    # function multiplicar dos numeros ingresados
+        print(f"Funcion multiplicar: {mult(random.randint(i, 100), random.randint(i, 100))}")
+    # funcion que genera una lista de numeros aleatorios de longitud variable
+        print(f"Funcion generacion lista numeros aleatorios: {generate_random_array(random.randint(i, 10))}")
+        print(f"fin de la prueba {i+1} \n")
